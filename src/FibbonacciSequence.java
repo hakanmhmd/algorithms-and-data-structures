6cc9ba428bce88f14f3acee1f0c3334236497f51
Hakan <hakanmhmd@yahoo.com>
2017-04-27 21:22:58 +0100

    remove frequecy count

--- src/FibbonacciSequence.java
+++ src/FibbonacciSequence.java
@@ -1,17 +1,18 @@
+/*
+    Implementation of fibonacci sequence with memoization
+ */
+
 public class FibbonacciSequence {
 
     private static long[] cache = new long[200];
-    private static long[] frequency = new long[200];
 
     public static void main(String[] args) {
         System.out.println(fibonacci(5));
-        printFrequency();
     }
 
-    // Dynamic programming implementation
+
     public static long fibonacci(int n){
         if(n == 0 || n == 1){
-            frequency[n]++;
             cache[n] = n;
             return cache[n];
         }
@@ -20,16 +21,7 @@ public class FibbonacciSequence {
             // number is not calculated yet
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2);
         }
-        // increment frequency
-        frequency[n]++;
 
         return cache[n];
-    }
-
-    public static void printFrequency(){
-        for (int i = 0; i < frequency.length; i++) {
-            long count = frequency[i];
-            System.out.println("n = " + i + ", count = " + count);
-        }
     }
 }
