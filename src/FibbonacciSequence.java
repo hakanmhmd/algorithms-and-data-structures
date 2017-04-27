d47db9f4b808a0080e9c593c333b6afb13697cae
Hakan <hakanmhmd@yahoo.com>
2017-04-27 20:42:10 +0100

    memoization

--- src/FibbonacciSequence.java
+++ src/FibbonacciSequence.java
@@ -1,12 +1,35 @@
 public class FibbonacciSequence {
+
+    private static long[] cache = new long[200];
+    private static long[] frequency = new long[200];
+
     public static void main(String[] args) {
-        System.out.println(fibbonacci(6));
+        System.out.println(fibonacci(5));
+        printFrequency();
     }
 
-    static long fibbonacci(long n){
-        if(n <= 1){
-            return n;
+    // Dynamic programming implementation
+    public static long fibonacci(int n){
+        if(n == 0 || n == 1){
+            frequency[n]++;
+            cache[n] = n;
+            return cache[n];
         }
-        return fibbonacci(n-1) + fibbonacci(n-2);
+
+        if (cache[n] == 0) {
+            // number is not calculated yet
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        // increment frequency
+        frequency[n]++;
+
+        return cache[n];
+    }
+
+    public static void printFrequency(){
+        for (int i = 0; i < frequency.length; i++) {
+            long count = frequency[i];
+            System.out.println("n = " + i + ", count = " + count);
+        }
     }
 }
