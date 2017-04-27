2c5b8ade2fb9434e93ac0de0e47a2f3e7271237d
Hakan <hakanmhmd@yahoo.com>
2017-04-27 19:36:40 +0100

    naive fib sequence

--- src/FibbonacciSequence.java
+++ src/FibbonacciSequence.java
@@ -1,3 +1,12 @@
 public class FibbonacciSequence {
+    public static void main(String[] args) {
+        System.out.println(fibbonacci(6));
+    }
 
+    static long fibbonacci(long n){
+        if(n <= 1){
+            return n;
+        }
+        return fibbonacci(n-1) + fibbonacci(n-2);
+    }
 }
