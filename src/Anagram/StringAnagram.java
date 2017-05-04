4fd2865ac6d375c60b38dcb4bd5faf6d485c91c2
Hakan <hakanmhmd@yahoo.com>
2017-05-04 10:35:04 +0100

    New chanllenges solved

---
+++ src/Anagram/StringAnagram.java
@@ -0,0 +1,7 @@
+package Anagram;
+
+/**
+ * Created by hakanmehmed on 03/05/2017.
+ */
+public class StringAnagram {
+}
---
+++ src/LongestPalindrome/PalindromeStringBuilder.java
@@ -0,0 +1,7 @@
+package LongestPalindrome;
+
+/**
+ * Created by hakanmehmed on 04/05/2017.
+ */
+public class PalindromeStringBuilder {
+}
