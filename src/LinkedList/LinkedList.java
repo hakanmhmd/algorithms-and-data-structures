5076d7eda2afea6fa42b6f8ebf633d06f9708a2c
Hakan <hakanmhmd@yahoo.com>
2017-05-05 23:23:05 +0100

    Simple linked list impl

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -5,9 +5,15 @@ package LinkedList;
  */
 public class LinkedList {
     // holds reference to the head
-    Node head;
+    private Node head;
+    private int listCount;
 
-    private static class Node {
+    public LinkedList() {
+        head = null;
+        listCount = 0;
+    }
+
+    private class Node {
         // each hode has data and pointer to the next
         int data;
         Node next;
@@ -15,5 +21,63 @@ public class LinkedList {
             this.data = data;
             next = null;
         }
+    }
+
+    // O(1)
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        listCount++;
+    }
+
+    // O(n)
+    // Optimization keep pointer to tail for O(1)
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = null;
+
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        listCount++;
+    }
+
+    public void addAfter(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public int size(){
+        return listCount;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addLast(0);
+
+        list.printList();
+        System.out.println(list.size());
     }
 }
